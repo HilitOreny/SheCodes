62c1c6a380da494a2b614c23f7310c68ae376861
Hilit Oreny <HilitOreny@users.noreply.github.com>
2020-10-31 16:37:49 +0200

    opening statement + import

---
+++ DustBusters.py
@@ -0,0 +1,8 @@
+import pandas
+"""this module is used to read excel files and work with databases"""
+
+print ("""Welcome to DustBusters!
+We help beginner programmers gain experience and skills.
+They volunteer to work on old open-source projects,
+and we guide them how to refactor and dust outdated code.
+Workers and volunteers, please use this system to mark your attendance in DustBusters projects.""")
