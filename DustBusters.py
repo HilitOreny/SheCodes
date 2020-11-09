7c2a0b0671e0cbd8ad18cb7ca6425ec3e9231785
Hilit Oreny <HilitOreny@users.noreply.github.com>
2020-11-09 21:52:12 +0200

    converting excel to dictionary

--- DustBusters.py
+++ DustBusters.py
@@ -1,5 +1,5 @@
-import pandas
-"""this module is used to read excel files and work with databases"""
+import pandas 
+"""pandas"""
 
 print ("""Welcome to DustBusters!
 We help beginner programmers gain experience and skills.
@@ -7,4 +7,10 @@ They volunteer to work on old open-source projects,
 and we guide them how to refactor and dust outdated code.
 Workers and volunteers, please use this system to mark your attendance in DustBusters projects.""")
 
-pandas.read_excel('add_data.xlsx', sheet_name='Sheet1')
+#create PIN and name dictionsry
+PIN_name = pandas.read_excel('add_data.xlsx',
+sheet_name='Sheet1', usecols = "A:B")
+
+PIN_name_dictionary = PIN_name.to_dict()
+
+print (PIN_name_dictionary)
