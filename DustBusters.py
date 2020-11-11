a86d6be1b4b78912dd89a15d71160fad0c41410e
Hilit Oreny <HilitOreny@users.noreply.github.com>
2020-11-11 21:20:28 +0200

    series to list. opening function.

--- DustBusters.py
+++ DustBusters.py
@@ -1,16 +1,21 @@
 import pandas 
-"""pandas"""
+"""pandas is used to read excel and CSV files, and convert them to SQL"""
 
-print ("""Welcome to DustBusters!
-We help beginner programmers gain experience and skills.
-They volunteer to work on old open-source projects,
-and we guide them how to refactor and dust outdated code.
-Workers and volunteers, please use this system to mark your attendance in DustBusters projects.""")
+def opening():
+    print("""Welcome to DustBusters!
+    We help beginner programmers gain experience and skills.
+    They volunteer to work on old open-source projects, and we guide them how to refactor and dust outdated code.
+    Workers and volunteers, please use this system to mark your attendance in DustBusters projects.""")
 
 #create PIN and name dictionsry
-PIN_name = pandas.read_excel('add_data.xlsx',
-sheet_name='Sheet1', usecols = "A:B")
+excel_data = pandas.read_excel('add_data.xlsx', sheet_name='Sheet1')
+num_list = excel_data['PIN'].tolist()
+names_list = excel_data['Full Name'].tolist()
+print(num_list)
+print(names_list)
 
-PIN_name_dictionary = PIN_name.to_dict()
+#dictionary comprehension
 
-print (PIN_name_dictionary)
+#given_num = input("Please enter your PIN: ")
+#given_name = input("Please enter your full name: ")
+
