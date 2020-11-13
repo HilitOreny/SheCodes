86db5051771a5825b05a26fb8398acb8a29c0575
Hilit Oreny <HilitOreny@users.noreply.github.com>
2020-11-13 09:32:54 +0200

    name series and list

--- Sustain/sustain_code_example.py
+++ Sustain/sustain_code_example.py
@@ -2,5 +2,7 @@ import pandas
 """pandas is used to read the excel file, and convert it to SQL"""
 
 excel_data = pandas.read_excel('Mock_Data.xlsx', sheet_name='Amazon')
+names_list = excel_data['Name'].tolist()
 membership_list = excel_data['Team Member'].tolist()
+print(names_list)
 print(membership_list)
