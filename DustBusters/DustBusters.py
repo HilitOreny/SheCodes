84c3e9cbb2288308190f6b6fe36ad05acad7b324
Hilit Oreny <HilitOreny@users.noreply.github.com>
2020-11-30 22:00:52 +0200

    continue PyInputPlus To Do: input validation (isalpha)

--- DustBusters/DustBusters.py
+++ DustBusters/DustBusters.py
@@ -6,7 +6,7 @@ import pyinputplus
 
 
 def opening():
-    print("""Welcome to DustBusters!
+    print("""Welcome to Dust Busters!
     We help beginner programmers gain experience and skills.
     They volunteer to work on old open-source projects, and we guide them how to refactor and dust outdated code.
     Workers and volunteers, please use this system to mark your attendance in DustBusters projects.""")
@@ -27,17 +27,21 @@ def input_validation(passwords_names_dict):
     :param passwords_names_dict: a dictionary of the participants names and their passwords
     :return: 
     """
-    given_num = pyinputplus.inputInt(prompt="Please enter your PIN: ", blank=False)
-    given_name = input("Please enter your full name: ")
-    if given_num in passwords_names_dict.keys():
-        print ("PIN found")
-    if given_name in passwords_names_dict.values():
-        print("Name found")
+    given_num = pyinputplus.inputInt(prompt="Please enter your PIN: ", min=100000, max=999999,
+                                     default=666, limit=1, timeout=5)
+    given_name = pyinputplus.inputStr(prompt="Please enter your full name: ",
+                                      default="Jane Doe", limit=1, timeout=5)
+    print ((given_num, given_name))
+    if (given_num, given_name) in passwords_names_dict.items():
+        print(given_name, "Welcome to Dust Busters")
+    else:
+        print("Dust Busters could not recognize the name or number you entered.")
+
 
 def run():
     """
     opens the file and applies the functions
-    :return:
+    :return: None
     """
     try:
         excel_content = pandas.read_excel('add_data.xlsx', sheet_name='Sheet1')
