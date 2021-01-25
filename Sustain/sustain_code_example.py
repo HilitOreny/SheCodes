215a5c451ea13f05c8b380085e3310a877cb2cff
Hilit Oreny <HilitOreny@users.noreply.github.com>
2021-01-25 20:21:30 +0200

    adding module docstring. add option to open different sheets. file formatting.

--- Sustain/sustain_code_example.py
+++ Sustain/sustain_code_example.py
@@ -1,14 +1,15 @@
+"""This module reads an excel file and formats it."""
+
 import pandas
 
-"""pandas is used to read the excel file and analyze the data"""
 
-
-def read_file_basic_python():
+def read_file(track_name):
     """
+    :param track_name: The name of the track, to be used as a sheet name.
     :return: a dataframe which contains the content of the Basic Python sheet in the excel file.
     """
     try:
-        file_content = pandas.read_excel("Mock_Data.xlsx", sheet_name="Basic Python")
+        file_content = pandas.read_excel("Mock_Data.xlsx", sheet_name=track_name)
     except ImportError:
         print("Python tried to open the file, but encountered a problem. Contact code maintainers")
     else:
@@ -16,7 +17,7 @@ def read_file_basic_python():
 
 
 def generate_data(raw_excel_data):
-    """this function reads the excel file and formats it by replacing empty cells with 0 and removing irrelevant data.
+    """this function replaces empty cells with 0 and removes irrelevant data.
     :param raw_excel_data: a dataframe which contains the raw data from the excel file.
     :unfiltered_data type: dataframe
     :return: a dataframe which only contains the relevant data.
@@ -24,9 +25,11 @@ def generate_data(raw_excel_data):
     filled_excel_data = raw_excel_data.fillna(0)
     no_team = filled_excel_data[filled_excel_data["Staff"] == "No"]
     drop = no_team.drop(columns=[
-        "Index", "Staff", "Email", "Joined", "Track", "Attendance in the last 10 weeks", "Max Lesson Entered"])
+        "Index", "Staff", "Email", "Joined", "Track",
+        "Attendance in the last 10 weeks", "Max Lesson Entered"])
     return drop
 
-excel_content = read_file_basic_python()
+
+excel_content = read_file("React")
 all_courses = generate_data(excel_content)
-print (all_courses)
+print(all_courses)
